3c0f2cb75b9ff04a76c573a60beeb9b45546ea56
shogo <shogo.baseball0306@gmail.com>
2024-03-12 21:27:47 +0900

    finish making hero section

---
+++ script/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    const cb = function (el, isIntersecting) {
+        if(isIntersecting) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    }
+
+    const so = new ScrollObserver('.tween-animate-title', cb);
+});
