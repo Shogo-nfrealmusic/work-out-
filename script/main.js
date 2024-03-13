ac10777f714486c42227456a824965125e732a98
shogo <shogo.baseball0306@gmail.com>
2024-03-13 18:19:13 +0900

    finish making mobile menu and header

--- script/main.js
+++ script/main.js
@@ -9,4 +9,17 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const so = new ScrollObserver('.tween-animate-title', cb);
+
+    const header = document.querySelector('.header');
+    const _navAnimation = function (el , inview) {
+        if(inview) {
+            header.classList.remove('triggered');
+        } else {
+            header.classList.add('triggered');
+        }
+    }
+
+    const so2 = new ScrollObserver('.nav-trigger', _navAnimation, {once:false});
+
+    new MobileMenu;
 });
