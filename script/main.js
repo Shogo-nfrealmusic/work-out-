cf8f1afdc4eb4f7537fd5ac9fc9060d1cab0099a
shogo <shogo.baseball0306@gmail.com>
2024-03-15 19:22:23 +0900

    finish making work out section

--- script/main.js
+++ script/main.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 
-    const cb = function (el, isIntersecting) {
-        if(isIntersecting) {
+    const _textAnimation = function (el, inview) {
+        if(inview) {
             const ta = new TweenTextAnimation(el);
             ta.animate();
         }
     }
 
-    const so = new ScrollObserver('.tween-animate-title', cb);
+    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
 
     const header = document.querySelector('.header');
     const _navAnimation = function (el , inview) {
@@ -22,4 +22,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const so2 = new ScrollObserver('.nav-trigger', _navAnimation, {once:false});
 
     new MobileMenu;
+
+    const _inviewAnimation = function (el, inview) {
+        if(inview) {
+            el.classList.add('inview');
+        } else {
+            el.classList.remove('inview');
+        }
+    }
+
+    const so3 = new ScrollObserver('.cover-slide', _inviewAnimation);
 });
